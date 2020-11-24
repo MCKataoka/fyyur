4eaa42a56c2b98b4e8dad04e4bc8127a57714975
MCKataoka <mario.c.kataoka@gmail.com>
2020-11-24 20:34:09 +0000

    Revised submission

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -15,12 +15,11 @@ from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
 import sys
-
+from models import app, db, Venue, Artist, Show
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
-app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -28,61 +27,7 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String))
-
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    facebook_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String)
-    image_link = db.Column(db.String(500))
-    genres = db.Column(db.ARRAY(db.String))
-
-
-class Show(db.Model):
-    __tablename__ = 'shows'
-    id = db.Column(db.Integer, primary_key=True)
-    start_time = db.Column(db.DateTime())
-    # Foreign Keys
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
-    # relationships
-    artist = db.relationship(
-        Artist,
-        backref=db.backref('shows', cascade='all, delete')
-    )
-    venue = db.relationship(
-        Venue,
-        backref=db.backref('shows', cascade='all, delete')
-    )
-
+app.config.from_object('models')
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -135,8 +80,6 @@ def venues():
             up_co = db.session.query(Show).filter(
                 Show.start_time > current_time, Show.venue_id == venues[j][0]).all()
             venue['num_upcoming_shows'] = len(up_co)
-
-    print('DATA:', data)
 
     return render_template('pages/venues.html', areas=data)
 
@@ -365,7 +308,6 @@ def show_artist(artist_id):
 def edit_artist(artist_id):
     form = ArtistForm()
     artist = Artist.query.filter_by(id=artist_id).first()
-    print('Artist: ', artist.name)
 
     return render_template('forms/edit_artist.html', form=form, artist=artist)
 
@@ -495,7 +437,6 @@ def shows():
         show.artist_name = artist.name
         show.artist_image_link = artist.image_link
         show.venue_name = venue.name
-        print('Artists Name:', show.artist_name)
 
     return render_template('pages/shows.html', shows=data)
 
---
+++ projects/01_fyyur/starter_code/models.py
@@ -0,0 +1,64 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
+
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
+    image_link = db.Column(db.String(500))
+    genres = db.Column(db.ARRAY(db.String))
+
+
+class Show(db.Model):
+    __tablename__ = 'shows'
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime())
+    # Foreign Keys
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
+    # relationships
+    artist = db.relationship(
+        Artist,
+        backref=db.backref('shows', cascade='all, delete')
+    )
+    venue = db.relationship(
+        Venue,
+        backref=db.backref('shows', cascade='all, delete')
+    )
